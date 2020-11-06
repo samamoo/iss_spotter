10de3083bcb276e572a8dfbb1f3fe9ef17bde105
Samantha Fok <samantha.fok@hotmail.com>
2020-11-06 06:27:04 +0000

    Fetch ISS Fly Over Times with coordinates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -8,3 +8,25 @@ const { fetchMyIP } = require('./iss');
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+//{ ip: '75.155.190.119' }
+
+// fetchCoordsByIP('75.155.190.119', (error, coords) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned Coords:' , coords);
+//   console.log(coords.latitude)
+// });
+
+const coords = { latitude: 49.2643, longitude: -123.0961 };
+fetchISSFlyOverTimes(coords, (error, data) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned Coords:' , data);
+});
