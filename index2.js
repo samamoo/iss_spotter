1df5aa6672c3e190c23b6249f1e815ca8466c3fc
Samantha Fok <samantha.fok@hotmail.com>
2020-11-06 08:24:03 +0000

    Refactored index.js to use promises and catch

---
+++ index2.js
@@ -0,0 +1,10 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised.js');
+const printPassTimes = require('./index.js')
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
